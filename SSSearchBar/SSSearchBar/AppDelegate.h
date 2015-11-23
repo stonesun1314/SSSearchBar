bb8f7d44a32340a09fac77449da823a008fe6897
stonesun1314 <lazycat-stone@LazyCatonedeMBP.lan>
2015-11-23 17:43:02 +0800

    第一次提交

---
+++ SSSearchBar/SSSearchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSSearchBar
+//
+//  Created by LazyCat-Stone on 15/11/23.
+//  Copyright © 2015年 LazyCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSSearchBar/SSSearchBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSSearchBar
+//
+//  Created by LazyCat-Stone on 15/11/23.
+//  Copyright © 2015年 LazyCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
